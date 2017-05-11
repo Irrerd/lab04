af22e38edcbfc56e98126b96a26c3de316cd2e91
u211-04 <u211-04@public.mpei.local>
2017-05-11 15:14:37 +0300

    punkt 1(2)

--- main.cpp
+++ main.cpp
@@ -45,9 +45,25 @@ void print_byte(uint8_t y){
     cout<<nibble_to_hex(high)<<nibble_to_hex(low);
 }
 
+void print_in_hex(const void* data, size_t size){
+    auto k = reinterpret_cast<const uint8_t*>(data);
+    for (size_t i = 0; i < size; i++ ){
+        print_byte(*(k+i));
+        cout<<';';
+    }
+}
+
 int
 main() {
     uint8_t z = 106;
     print_byte(z);
+    cout<<'\n';
+
+    int l = 69;
+    print_in_hex(&l, sizeof(l));
+
+
     return 0;
+
+
 }
